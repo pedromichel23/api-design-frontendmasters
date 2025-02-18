edecd18843d14e59b2cd2ae53ad6b3a25c7f8b34
Pedro Michel <pedromichel23@gmail.com>
2025-02-18 10:00:49 -0700

    add express validator and product middleware

--- src/router.ts
+++ src/router.ts
@@ -1,13 +1,25 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
+import { body, oneOf, validationResult } from 'express-validator'
+import { handlerInputsErrors } from "./modules/middlewares";
 
 export const router = Router()
+
+enum UPDATE_STATUS {
+    IN_PROGRESS,
+    SHIPPED,
+    DEPRECATED
+}
 
 // Product
 router.get('/product', (req, res) => {
     res.json({ msg: 'Hello from product' })
 })
 router.get('/product/:id', () => { })
-router.put('/product/:id', () => { })
+router.put('/product/:id', body('name').exists().isString(), handlerInputsErrors, (req: Request, res: Response) => {
+
+    res.json({ msg: "Ok" })
+
+})
 router.post('/product', () => { })
 router.delete('/product/:id', () => { })
 
@@ -16,7 +28,16 @@ router.delete('/product/:id', () => { })
 // Update
 router.get('/update', () => { })
 router.get('/update/:id', () => { })
-router.put('/update/:id', () => { })
+router.put('/update/:id',
+    body('title').optional(),
+    body('body').optional(),
+    body('status').optional()
+        .isIn([UPDATE_STATUS.DEPRECATED,
+        UPDATE_STATUS.IN_PROGRESS,
+        UPDATE_STATUS.SHIPPED]
+        ),
+    body('version').optional(),
+    () => { })
 router.post('/update', () => { })
 router.delete('/update/:id', () => { })
 
